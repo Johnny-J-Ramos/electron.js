32e6aaa9d2885715be8d8753251a12d3dea0330c
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-12-17 23:10:39 -0300

    iniciando app com electronJs

---
+++ src/Notifyer.js
@@ -0,0 +1,9 @@
+const Notifyer = {
+  async init() {
+      console.log(Notification)
+   },
+   notify() {}
+ 
+ }
+ 
+ export { Notifyer }
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { Notifyer } from './Notifyer.js'
+
+const app = {
+  start() {
+     Notifyer.init()
+  } 
+}
+
+export { app }
